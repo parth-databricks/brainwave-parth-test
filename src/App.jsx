9f25bf50298a283d4d7915633dc2eed72eab25c0
parthusun8 <parthusun8@gmail.com>
2024-11-03 21:17:03 +0530

    Button Done, Starting Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
 
       <div className="pt-[4.75rem] lg:pt-[5.24rem] overflow-hidden">
-        <Button className="mt-10" href="login">
-          <ButtonGradient />
-        </Button>
+        <Header />
+
+        {/* <Button className="mt-10" href="#login">
+          Something
+        </Button> */}
       </div>
+      <ButtonGradient />
     </>
   );
 }
